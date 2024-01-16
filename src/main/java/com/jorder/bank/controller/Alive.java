6edb8f990ce6b1670ef1615970fc64fc828a3b28
JorderGomes <jordernoveau@gmail.com>
2024-01-16 16:30:18 -0300

    #main - initial commit

---
+++ src/main/java/com/jorder/bank/controller/Alive.java
@@ -0,0 +1,18 @@
+package com.jorder.bank.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/alive")
+public class Alive {
+    
+    @GetMapping("")
+    public String alive() {
+        return "Alive";
+    }
+    
+
+}
