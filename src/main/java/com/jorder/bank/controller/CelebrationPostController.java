4529e67f2adc5a1867ad9d50e8c89c0ba480f700
JorderGomes <jordernoveau@gmail.com>
2024-01-17 21:12:45 -0300

    #main - add controllers to talent, schedule and celebrationPost

---
+++ src/main/java/com/jorder/bank/controller/CelebrationPostController.java
@@ -0,0 +1,68 @@
+package com.jorder.bank.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jorder.bank.model.CelebrationPost;
+import com.jorder.bank.repository.CelebrationPostRepository;
+import com.jorder.bank.service.CelebrationPostService;
+
+@RestController
+@RequestMapping("/celebrationPost")
+public class CelebrationPostController {
+    
+    @Autowired
+    private CelebrationPostService celebrationPostService;
+
+    @Autowired
+    private CelebrationPostRepository celebrationPostRepository;
+
+    @GetMapping
+    public ResponseEntity<List<CelebrationPost>> getCelebrationPosts(){
+        return ResponseEntity.ok(celebrationPostService.getCelebrationPosts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CelebrationPost> getCelebrationPost(@PathVariable Long id){
+        return celebrationPostService.getCelebrationPostById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CelebrationPost postCelebrationPost(@RequestBody CelebrationPost celebrationPost){
+        return celebrationPostService.createCelebrationPost(celebrationPost);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteCelebrationPost(@PathVariable Long id){
+        if (!celebrationPostRepository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+        celebrationPostRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<CelebrationPost> updateCelebrationPost(@RequestBody CelebrationPost celebrationPost, @PathVariable Long id){
+        CelebrationPost updatedCelebrationPost = celebrationPostService.editCelebrationPost(id, celebrationPost);
+        if (updatedCelebrationPost == null){
+            return ResponseEntity.notFound().build();
+        }
+        return  ResponseEntity.ok(updatedCelebrationPost);
+    }
+
+}
---
+++ src/main/java/com/jorder/bank/controller/TalentController.java
@@ -0,0 +1,68 @@
+package com.jorder.bank.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jorder.bank.model.Talent;
+import com.jorder.bank.repository.TalentRepository;
+import com.jorder.bank.service.TalentService;
+
+@RestController
+@RequestMapping("/talent")
+public class TalentController {
+    
+    @Autowired
+    private TalentService talentService;
+
+    @Autowired
+    private TalentRepository talentRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Talent>> getTalents(){
+        return ResponseEntity.ok(talentService.getTalents());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Talent> getTalent(@PathVariable Long id){
+        return talentService.getTalentById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Talent postTalent(@RequestBody Talent talent){
+        return talentService.createTalent(talent);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteTalent(@PathVariable Long id){
+        if (!talentRepository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+        talentRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Talent> updateTalent(@RequestBody Talent talent, @PathVariable Long id){
+        Talent updatedTalent = talentService.editTalent(id, talent);
+        if (updatedTalent == null){
+            return ResponseEntity.notFound().build();
+        }
+        return  ResponseEntity.ok(updatedTalent);
+    }
+
+}
