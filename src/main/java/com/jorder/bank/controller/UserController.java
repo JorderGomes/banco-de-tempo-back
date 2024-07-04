ad457d37ffa3c93a60011e26c6b90271ddd421ab
JorderGomes <jordernoveau@gmail.com>
2024-07-04 17:53:45 -0300

    feat: update balance

--- src/main/java/com/jorder/bank/controller/UserController.java
+++ src/main/java/com/jorder/bank/controller/UserController.java
@@ -7,11 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +41,15 @@ public class UserController {
         return userService.getUserById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PatchMapping("/{id}/update-balance")
+    public ResponseEntity<User> updateBalance(@PathVariable Long id, @RequestParam int amount){
+        var userUpdated =  userService.updateBalance(id, amount);
+        if (userUpdated == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(userUpdated);
     }
 
     @PostMapping
